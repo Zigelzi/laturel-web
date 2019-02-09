181f021866e78d2a5802d76b981e2d855883bee3
Zigelzi <40773205+Zigelzi@users.noreply.github.com>
2019-02-09 18:32:12 +0200

    renamed app and moved forms to forms.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, request
-from chargers import ChargerForm
-
+from forms import ChargerForm
+from config import Config
 
 app = Flask(__name__)
-
-app.config['SECRET_KEY'] = 'debug'
+app.config.from_object(Config)
 
 @app.route('/')
 def index():
--- forms.py
+++ forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, DateTimeField
 
 class ChargerForm(FlaskForm):
     name = StringField('Test Name')
+    date1 = DateTimeField('Departure time', format='%Y-%m')
     submit = SubmitField('Check how powerful charger I need!')
