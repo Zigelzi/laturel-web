fcf5272320e5f79f85aad042f7902a77dbb2a958
Zigelzi <40773205+Zigelzi@users.noreply.github.com>
2019-02-10 12:40:32 +0200

    Reformatted forms

--- forms.py
+++ forms.py
@@ -2,10 +2,9 @@ from flask_wtf import FlaskForm
 from wtforms import IntegerField, SubmitField, DateTimeField, RadioField
 
 class ChargerForm(FlaskForm):
-    driveKmStr = IntegerField('How much do you drive daily?')
     driveKmRadio = RadioField('How much do you drive daily?',
-                              choices=[('50', '0 - 50'),
-                                       ('100', '51 - 100')])
+                              choices=[('50', '0 - 50 km'),
+                                       ('100', '51 - 100 km')])
     radio = RadioField('When do you stop your driving?', choices=[('18:00', '18:00'), ('19:00', '19:00')])
     radio2 = RadioField('When do you start driving?', choices=[('05:00', '05:00'), ('06:00', '06:00')])
     submit = SubmitField('Check how powerful charger I need!')
